901e3f8cc4229e73ed792bfb3c07b23aa2566a2c
liebero3 <liebero3@gmail.com>
2022-02-21 09:38:28 +0100

    changed format of all functions

--- forms.py
+++ forms.py
@@ -36,8 +36,10 @@ def listOwnedForms():
     endpoint = '/api/v1.1/forms'
     url = NEXTCLOUD_URL + NEXTCLOUD_APP + endpoint
     headers = {'OCS-APIRequest': 'true'}
-    r = requests.get(url, auth=(NEXTCLOUD_USERNAME,
-                     NEXTCLOUD_PASSWORD), headers=headers)
+    r = requests.get(url,
+                     auth=(NEXTCLOUD_USERNAME, NEXTCLOUD_PASSWORD),
+                     headers=headers
+                     )
     return r
 
 
@@ -55,8 +57,10 @@ def listSharedForms():
     endpoint = '/api/v1.1/shared_forms'
     url = NEXTCLOUD_URL + NEXTCLOUD_APP + endpoint
     headers = {'OCS-APIRequest': 'true'}
-    r = requests.get(url, auth=(NEXTCLOUD_USERNAME,
-                     NEXTCLOUD_PASSWORD), headers=headers)
+    r = requests.get(url,
+                     auth=(NEXTCLOUD_USERNAME, NEXTCLOUD_PASSWORD),
+                     headers=headers
+                     )
     return r
 
 
@@ -86,8 +90,10 @@ def createNewForm():
     endpoint = '/api/v1.1/form'
     url = NEXTCLOUD_URL + NEXTCLOUD_APP + endpoint
     headers = {'OCS-APIRequest': 'true'}
-    r = requests.post(url, auth=(NEXTCLOUD_USERNAME,
-                      NEXTCLOUD_PASSWORD), headers=headers)
+    r = requests.post(url,
+                      auth=(NEXTCLOUD_USERNAME, NEXTCLOUD_PASSWORD),
+                      headers=headers
+                      )
     return r
 
 
@@ -155,8 +161,10 @@ def requestFullDataOfAForm(id: int):
     endpoint = '/api/v1.1/form'
     url = NEXTCLOUD_URL + NEXTCLOUD_APP + endpoint + f'/{id}'
     headers = {'OCS-APIRequest': 'true'}
-    r = requests.get(url, auth=(NEXTCLOUD_USERNAME,
-                     NEXTCLOUD_PASSWORD), headers=headers)
+    r = requests.get(url,
+                     auth=(NEXTCLOUD_USERNAME,  NEXTCLOUD_PASSWORD),
+                     headers=headers
+                     )
     return r
 
 
@@ -176,8 +184,10 @@ def cloneAForm(id: int):
     endpoint = '/api/v1.1/form/clone'
     url = NEXTCLOUD_URL + NEXTCLOUD_APP + endpoint + f'/{id}'
     headers = {'OCS-APIRequest': 'true'}
-    r = requests.post(url, auth=(NEXTCLOUD_USERNAME,
-                      NEXTCLOUD_PASSWORD), headers=headers)
+    r = requests.post(url,
+                      auth=(NEXTCLOUD_USERNAME, NEXTCLOUD_PASSWORD),
+                      headers=headers
+                      )
     return r
 
 
@@ -206,8 +216,11 @@ def updateFormProperties(data: dict):
         'OCS-APIRequest': 'true',
         # 'Content-Type': 'application/x-www-form-urlencoded'
     }
-    r = requests.post(url, auth=(NEXTCLOUD_USERNAME,
-                      NEXTCLOUD_PASSWORD), headers=headers, json=data)
+    r = requests.post(url,
+                      auth=(NEXTCLOUD_USERNAME, NEXTCLOUD_PASSWORD),
+                      headers=headers,
+                      json=data
+                      )
     return r
 
 
@@ -224,8 +237,219 @@ def deleteAForm(id: int, keyValuePairs: dict):
     endpoint = '/api/v1.1/form'
     url = NEXTCLOUD_URL + NEXTCLOUD_APP + endpoint + f'/{id}'
     headers = {'OCS-APIRequest': 'true'}
-    r = requests.delete(url, auth=(NEXTCLOUD_USERNAME,
-                                   NEXTCLOUD_PASSWORD), headers=headers)
+    r = requests.delete(url,
+                        auth=(NEXTCLOUD_USERNAME, NEXTCLOUD_PASSWORD),
+                        headers=headers
+                        )
+    return r
+
+
+def createANewQuestion(data: dict):
+    '''
+    Endpoint: /api/v1.1/question
+    Method: POST
+    Parameters:
+    Parameter	Type	       Optional	Description
+    formId	    Integer		            ID of the form, the new question will belong to
+    type	    QuestionType		    The question-type of the new question
+    text	    String	       yes	    Optional The text of the new question.
+    Response: The new question object.
+    "data": {
+    "id": 3,
+    "formId": 3,
+    "order": 3,
+    "type": "short",
+    "mandatory": false, // deprecated, will be removed in API v2
+    "isRequired": false,
+    "text": "",
+    "options": []
+    }
+
+    Question Types
+    Currently supported Question-Types are:
+
+    Type-ID	            Description
+    multiple	        Typically known as 'Checkboxes'. Using pre-defined options, the user can select one or multiple from. Needs at least one option available.
+    multiple_unique	    Typically known as 'Radio Buttons'. Using pre-defined options, the user can select exactly one from. Needs at least one option available.
+    dropdown	        Similar to multiple_unique, but rendered as dropdown field.
+    short	            A short text answer. Single text line
+    long	            A long text answer. Multi-line supported
+    date	            Showing a dropdown calendar to select a date.
+    datetime	        Showing a dropdown calendar to select a date and a time.
+    '''
+    endpoint = '/api/v1.1/question'
+    url = NEXTCLOUD_URL + NEXTCLOUD_APP + endpoint
+    headers = {
+        'OCS-APIRequest': 'true',
+        # 'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    r = requests.post(url,
+                      auth=(NEXTCLOUD_USERNAME, NEXTCLOUD_PASSWORD),
+                      headers=headers,
+                      json=data
+                      )
+    return r
+
+
+def updateQuestionProperties(data: dict):
+    '''
+    Update a single or multiple properties of a question-object.
+
+    Endpoint: /api/v1.1/question/update
+    Method: POST
+    Parameters:
+    Parameter	Type	Description
+    id	Integer	ID of the question to update
+    keyValuePairs	Array	Array of key-value pairs to update
+    Restrictions: It is not allowed to update one of the following 
+    key-value pairs: id, formId, order.
+    Response: Status-Code OK, as well as the id of the updated question.
+    "data": 1
+    '''
+    endpoint = '/api/v1.1/question/update'
+    url = NEXTCLOUD_URL + NEXTCLOUD_APP + endpoint
+    headers = {
+        'OCS-APIRequest': 'true',
+        # 'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    r = requests.post(url,
+                      auth=(NEXTCLOUD_USERNAME, NEXTCLOUD_PASSWORD),
+                      headers=headers,
+                      json=data
+                      )
+    return r
+
+
+def reorderQuestions(data: dict):
+    '''
+    Reorders all Questions of a single form
+
+    Endpoint: /api/v1.1/question/reorder
+    Method: POST
+    Parameters:
+    Parameter	Type	Description
+    formId	    Integer	ID of the form, the questions belong to
+    newOrder	Array	Array of all Question-IDs, ordered in the desired order
+    Restrictions: The Array must contain all Question-IDs corresponding to the specified form and must not contain any duplicates.
+    Response: Array of questionIDs and their corresponding order.
+    "data": {
+    "1": {
+        "order": 1
+    },
+    "2": {
+        "order": 3
+    },
+    "3": {
+        "order": 2
+    }
+    }
+    '''
+    endpoint = '/api/v1.1/question/reorder'
+    url = NEXTCLOUD_URL + NEXTCLOUD_APP + endpoint
+    headers = {
+        'OCS-APIRequest': 'true',
+        # 'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    r = requests.post(url,
+                      auth=(NEXTCLOUD_USERNAME, NEXTCLOUD_PASSWORD),
+                      headers=headers,
+                      json=data
+                      )
+    return r
+
+
+def deleteAQuestion(id: int):
+    '''
+    Endpoint: /api/v1.1/question/{id}
+    Url-Parameter:
+    Parameter	Type	Description
+    id	        Integer	ID of the question to delete
+    Method: DELETE
+    Response: Status-Code OK, as well as the id of the deleted question.
+    "data": 4
+    '''
+    endpoint = '/api/v1.1/question'
+    url = NEXTCLOUD_URL + NEXTCLOUD_APP + endpoint + f'/{id}'
+    headers = {
+        'OCS-APIRequest': 'true',
+        # 'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    r = requests.delete(url,
+                        auth=(NEXTCLOUD_USERNAME, NEXTCLOUD_PASSWORD),
+                        headers=headers
+                        )
+    return r
+
+
+def createANewOption(data: dict):
+    '''
+    Contains only manipulative question-endpoints. To retrieve options, request the full form data.
+
+    Create a new Option
+    Endpoint: /api/v1.1/option
+    Method: POST
+    Parameters:
+    Parameter	Type	Description
+    questionId	Integer	ID of the question, the new option will belong to
+    text	    String	The text of the new option
+    Response: The new option object
+    "data": {
+    "id": 7,
+    "questionId": 1,
+    "text": "test"
+    }
+    '''
+    endpoint = '/api/v1.1/option'
+    url = NEXTCLOUD_URL + NEXTCLOUD_APP + endpoint
+    headers = {'OCS-APIRequest': 'true'}
+    r = requests.post(url,
+                      auth=(NEXTCLOUD_USERNAME, NEXTCLOUD_PASSWORD),
+                      headers=headers,
+                      json=data
+                      )
+    return r
+
+
+def updateOptionProperties(data: dict):
+    '''
+    Endpoint: /api/v1.1/option/update
+    Method: POST
+    Parameters:
+    Parameter	    Type	Description
+    id	            Integer	ID of the option to update
+    keyValuePairs	Array	Array of key-value pairs to update
+    Restrictions: It is not allowed to update one of the following key-value pairs: id, questionId.
+    Response: Status-Code OK, as well as the id of the updated option.
+    "data": 7
+    '''
+    endpoint = '/api/v1.1/option/update'
+    url = NEXTCLOUD_URL + NEXTCLOUD_APP + endpoint
+    headers = {'OCS-APIRequest': 'true'}
+    r = requests.post(url,
+                      auth=(NEXTCLOUD_USERNAME, NEXTCLOUD_PASSWORD),
+                      headers=headers,
+                      json=data
+                      )
+    return r
+
+
+def deleteAnOption(data: dict):
+    '''
+    Endpoint: /api/v1.1/option/{id}
+    Url-Parameter:
+    Parameter	Type	Description
+    id	        Integer	ID of the option to delete
+    Method: DELETE
+    Response: Status-Code OK, as well as the id of the deleted option.
+    "data": 7
+    '''
+    endpoint = '/api/v1.1/option'
+    url = NEXTCLOUD_URL + NEXTCLOUD_APP + endpoint + f'/{id}'
+    headers = {'OCS-APIRequest': 'true'}
+    r = requests.delete(url,
+                        auth=(NEXTCLOUD_USERNAME, NEXTCLOUD_PASSWORD),
+                        headers=headers
+                        )
     return r
 
 
@@ -246,8 +470,10 @@ def getSubmissionsAsCSV(formshash: str):
     endpoint = '/api/v1.1/submissions/export'
     url = NEXTCLOUD_URL + NEXTCLOUD_APP + endpoint + f'/{formshash}'
     headers = {'OCS-APIRequest': 'true'}
-    r = requests.get(url, auth=(NEXTCLOUD_USERNAME,
-                     NEXTCLOUD_PASSWORD), headers=headers)
+    r = requests.get(url,
+                     auth=(NEXTCLOUD_USERNAME, NEXTCLOUD_PASSWORD),
+                     headers=headers
+                     )
     return r
 
 
@@ -277,18 +503,20 @@ def exportSubmissionsToCloud(formshash: str, path: str):
 
 
 if __name__ == "__main__":
-    # print(getFormData(NEXTCLOUD_FORM).text)
-    # print(getFormsOfUser().text)
-    print(requestFullDataOfAForm(9).text)
-    # print(createNewForm().text)
-    # data={
-    #     "id":9,
-    #     "keyValuePairs":
-    #         {
-    #         "title":"Testtitel",
-    #         "description":"Dies ist eine Testform"
-    #         }
-    #     }
-    # print(updateFormProperties(data).text)
-
+    # listOwnedForms()
+    # listSharedForms()
+    # createNewForm()
+    # requestFullDataOfAForm()
+    # cloneAForm()
+    # updateFormProperties()
+    # deleteAForm()
+    # createANewQuestion()
+    # updateQuestionProperties()
+    # reorderQuestions()
+    # deleteAQuestion()
+    # createANewOption()
+    # updateOptionProperties()
+    # deleteAnOption()
+    # getSubmissionsAsCSV()
+    # exportSubmissionsToCloud()
     pass
