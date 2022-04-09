425fee984c6d16fdfe43b068d04f5e812e94f71b
liebero3 <liebero3@gmail.com>
2022-04-09 22:16:25 +0200

    changed to .json() everywhere

--- schadensmeldung.py
+++ schadensmeldung.py
@@ -4,7 +4,6 @@ from deck import *
 from forms import *
 import json
 import csv
-from types import SimpleNamespace
 from datetime import datetime, timezone
 
 NEXTCLOUD_URL_DECK = credentials.url2
@@ -32,11 +31,8 @@ class NewSubmission():
 def getAllSubmissions():
     ''' get all submissions and save them as python object'''
     # save json to variable
-    data = getFormSubmissions('WgTCRZwyi6WfTEa2').text
-
-    # transform json to python object
-    x = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
-    return x
+    data = getFormSubmissions('WgTCRZwyi6WfTEa2').json()
+    return data
 
 
 def getHash(file):
@@ -55,26 +51,26 @@ def setHash(file):
         spamwriter.writerow(answers)
 
 
-def getNewSubmissions(file, x):
+def getNewSubmissions(file, data):
     ''' Get all submissions'''
-    for answer in x.ocs.data.submissions:
+    for answer in data["ocs"]["data"]["submissions"]:
         # Check if submission is already in known answers
-        if str(answer.id) in answers:
-            print(f"{str(answer.id)} ist schon in der Liste")
+        if str(answer["id"]) in answers:
+            print(f'{answer["id"]} ist schon in der Liste')
         else:
             # Submission is new, so it gets appended to known anwers and saved in file
-            print(f"Beitrag mit id {answer.id} ist neu und wird hinzugefügt.")
-            answers.append(answer.id)
+            print(f'Beitrag mit id {answer["id"]} ist neu und wird hinzugefügt.')
+            answers.append(answer["id"])
             setHash(file)
-            print(answer.answers[0].text)  # text
-            print(answer.answers[1].text)  # type
-            print(answer.answers[2].text)  # room
-            print(answer.timestamp)  # timestamp
-            print(answer.userId)  # userId
-            utc_time = datetime.fromtimestamp(answer.timestamp, timezone.utc)
+            print(answer["answers"][0]["text"])  # text
+            print(answer["answers"][1]["text"])  # type
+            print(answer["answers"][2]["text"])  # room
+            print(answer["timestamp"])  # timestamp
+            print(answer["userId"])  # userId
+            utc_time = datetime.fromtimestamp(answer["timestamp"], timezone.utc)
             local_time = utc_time.astimezone().strftime("%Y-%m-%d %H:%M:%S")
             errors.append(NewSubmission(
-                answer.userId, answer.answers[1].text, answer.answers[0].text, answer.answers[2].text, local_time))
+                answer["userId"], answer["answers"][1]["text"], answer["answers"][0]["text"], answer["answers"][2]["text"], local_time))
     for sub in errors:
         print(sub.author, sub.type, sub.text, sub.location, sub.timestamp)
 
@@ -85,13 +81,14 @@ if __name__ == '__main__':
     file = "answers.csv"
     answers = getHash(file)
     errors = []
-    x = getAllSubmissions()
-    getNewSubmissions(file, x)
+    data = getAllSubmissions()
+    getNewSubmissions(file, data)
     # r = getAListOfBoards({"details":"true"}).json()
     # for item in r:
     #     print(json.dumps(item, indent=2))
 
     board = getCardDetails(42,121,272).json()
     print(json.dumps(board, indent=2))
+
     # for item in board:
     #     print(json.dumps(item, indent=2))
